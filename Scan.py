c706af0d5cac92c5a235e4c6940ec0815a65ebef
Camilo Suarez <juan.suarez.quinones@unillanos.edu.co>
2020-10-16 14:42:34 -0500

    importaciones y variables globales

---
+++ Scan.py
@@ -0,0 +1,22 @@
+import subprocess
+import matplotlib
+import sys
+matplotlib.use('Qt5Agg')
+import numpy as np
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QGridLayout, QWidget, QLabel
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as fc
+from matplotlib.figure import Figure 
+
+canal=[]
+frecuencia =[]
+señal=[]
+essid=[]
+lim = np.arange(-0.5,0.5,0.001)
+l = np.linspace(0,np.pi,len(lim))
+si = np.sin(l)
+
+class MplCanvas(fc):
+    def __init__(self, parent=None, width=8, height=5, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
